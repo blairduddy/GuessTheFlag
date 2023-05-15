943ea6bccc9984c10546656e8dfdf839f1c7a9b7
Blair Duddy <blairduddy@hotmail.co.uk>
2023-05-15 08:14:01 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Blair Duddy on 2023-05-15.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
